1181df93f46a8244b740e6755290d1abd567ece4
Shogo Otake <shogo.otake@gmail.com>
2018-03-22 00:11:03 +0900

    Add plamo_http_headers_for_each

--- src/plamo_http_headers.rs
+++ src/plamo_http_headers.rs
@@ -1,2 +1,14 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
 #[repr(C)]
-pub struct PlamoHttpHeaders(Vec<String>);
+pub struct PlamoHttpHeaders {
+    inner: Vec<CString>,
+}
+
+#[no_mangle]
+pub extern fn plamo_http_headers_for_each(plamo_http_headers: *const PlamoHttpHeaders, callback: extern "C" fn(*const c_char)) {
+    unsafe {
+        (*plamo_http_headers).inner.iter().for_each(|header| callback(header.as_ptr()));
+    }
+}
