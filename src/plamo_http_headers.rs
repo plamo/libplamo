d619e8a1cfaad2cf83bb00080562f210412ebd23
Shogo Otake <shogo.otake@gmail.com>
2018-03-25 10:36:52 +0900

    Add fn plamo_http_headers_append

--- src/plamo_http_headers.rs
+++ src/plamo_http_headers.rs
@@ -1,4 +1,4 @@
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
 #[repr(C)]
@@ -10,5 +10,12 @@ pub struct PlamoHttpHeaders {
 pub extern fn plamo_http_headers_for_each(plamo_http_headers: *const PlamoHttpHeaders, callback: extern "C" fn(*const c_char)) {
     unsafe {
         (*plamo_http_headers).inner.iter().for_each(|header| callback(header.as_ptr()));
+    }
+}
+
+#[no_mangle]
+pub extern fn plamo_http_headers_append(plamo_http_headers: *mut PlamoHttpHeaders, value: *const c_char) {
+    unsafe {
+        (*plamo_http_headers).inner.push(CString::from(CStr::from_ptr(value)));
     }
 }
