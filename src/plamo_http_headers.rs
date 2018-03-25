17ae2025c9a028f4394c8173a0984e44627b7501
Shogo Otake <shogo.otake@gmail.com>
2018-03-25 11:00:50 +0900

    Add fn plamo_http_headers_remove

--- src/plamo_http_headers.rs
+++ src/plamo_http_headers.rs
@@ -1,3 +1,4 @@
+use plamo_result::PlamoResult;
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
@@ -17,5 +18,18 @@ pub extern fn plamo_http_headers_for_each(plamo_http_headers: *const PlamoHttpHe
 pub extern fn plamo_http_headers_append(plamo_http_headers: *mut PlamoHttpHeaders, value: *const c_char) {
     unsafe {
         (*plamo_http_headers).inner.push(CString::from(CStr::from_ptr(value)));
+    }
+}
+
+#[no_mangle]
+pub extern fn plamo_http_headers_remove(plamo_http_headers: *mut PlamoHttpHeaders, index: usize) -> PlamoResult {
+    unsafe {
+        match (*plamo_http_headers).inner.get(index) {
+            Some(_) => {
+                (*plamo_http_headers).inner.remove(index);
+                PlamoResult::Ok
+            },
+            None => PlamoResult::NotFound,
+        }
     }
 }
