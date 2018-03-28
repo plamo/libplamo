a64f87c462436e21dc708c0c8a894c978419b51f
Shogo Otake <shogo.otake@gmail.com>
2018-03-29 00:39:47 +0900

    Add fn plamo_response_get_status_XXX

--- src/plamo_response.rs
+++ src/plamo_response.rs
@@ -27,6 +27,20 @@ pub extern fn plamo_response_destroy(plamo_response: &mut *mut PlamoResponse) {
 }
 
 #[no_mangle]
+pub extern fn plamo_response_get_status_code(plamo_response: *const PlamoResponse) -> c_uint {
+    unsafe {
+        (*plamo_response).status_code
+    }
+}
+
+#[no_mangle]
+pub extern fn plamo_response_get_header(plamo_response: *mut PlamoResponse) -> *mut PlamoHttpHeader {
+    unsafe {
+        &mut (*plamo_response).header
+    }
+}
+
+#[no_mangle]
 pub extern fn plamo_response_set_body(plamo_response: *mut PlamoResponse, body: *mut c_uchar, size: usize) {
     unsafe {
         (*plamo_response).body = Vec::from_raw_parts(body, size, size);
