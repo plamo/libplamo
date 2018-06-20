5753452f17ff79912e87f9a0a9db49780815f3db
Shogo Otake <shogo.otake@gmail.com>
2018-06-21 02:25:22 +0900

    Add plamo_http_query_add

--- src/plamo_http_query.rs
+++ src/plamo_http_query.rs
@@ -26,6 +26,18 @@ pub extern fn plamo_http_query_get(plamo_http_query: *const PlamoHttpQuery, key:
 }
 
 #[no_mangle]
+pub extern fn plamo_http_query_add(plamo_http_query: *mut PlamoHttpQuery, key: *const c_char, value: *const c_char) {
+    unsafe {
+        match (*plamo_http_query).inner.get_mut(CStr::from_ptr(key)) {
+            Some(plamo_string_array) => plamo_string_array.inner.push(CString::from_raw(value as *mut _)),
+            None => {
+                (*plamo_http_query).inner.insert(CString::from_raw(key as *mut _), PlamoStringArray { inner: vec![CString::from_raw(value as *mut _)]} );
+            },
+        }
+    }
+}
+
+#[no_mangle]
 pub extern fn plamo_http_query_remove(plamo_http_query: *mut PlamoHttpQuery, key: *const c_char) -> bool {
     unsafe {
         (*plamo_http_query).inner.remove(CStr::from_ptr(key)).is_some()
